808ee222ed2c229a4930a9842fa8ecf47b10205f
Nitto <nittosantos.dev@gmail.com>
2024-02-19 23:00:10 -0300

    feat: :sparkles: adicionando transição

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -6,10 +6,14 @@ import { useRouter } from "next/router";
 import { AnimatePresence, motion } from "framer-motion";
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
   return (
     <Layout>
       <AnimatePresence mode="wait">
-        <Component {...pageProps} />
+        <motion.div key={router.route} className="h-full">
+          <Transition />
+          <Component {...pageProps} />
+        </motion.div>
       </AnimatePresence>
     </Layout>
   );
