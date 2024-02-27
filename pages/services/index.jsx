8c8edb98651fd43764b469979fee9d3d63b5ac71
Nitto <nittosantos.dev@gmail.com>
2024-02-27 17:46:37 -0300

    feat: :sparkles: finalize page to services

--- pages/services/index.jsx
+++ pages/services/index.jsx
@@ -5,42 +5,6 @@ import Circles from "../../components/Circles";
 import { motion } from "framer-motion";
 import { fadeIn } from "../../variants";
 
-import {
-  RxCrop,
-  RxPencil2,
-  RxDesktop,
-  RxReader,
-  RxRocket,
-} from "react-icons/rx";
-
-export const serviceData = [
-  {
-    icon: <RxCrop />,
-    title: "Branding",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-  },
-  {
-    icon: <RxPencil2 />,
-    title: "Design",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-  },
-  {
-    icon: <RxDesktop />,
-    title: "Development",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-  },
-  {
-    icon: <RxReader />,
-    title: "Copywritting",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-  },
-  {
-    icon: <RxRocket />,
-    title: "SEO",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-  },
-];
-
 const Services = () => {
   return (
     <div className="h-full bg-primary/30 py-36 flex items-center">
@@ -49,17 +13,37 @@ const Services = () => {
         <div className="flex flex-col xl:flex-row gap-x-8">
           {/* text */}
           <div className="text-center flex xl:w-[30vw] flex-col lg:text-left mb-4 xl:mb-0">
-            <h2 className="h2 xl:mt-8">
+            <motion.h2
+              variants={fadeIn("up", 0.3)}
+              initial="hidden"
+              animate="show"
+              exit="hidden"
+              className="h2 xl:mt-8"
+            >
               My services <span className="text-accent">.</span>
-            </h2>
-            <p className="mb-4 max-w-[400px] mx-auto lg:mx-0">
+            </motion.h2>
+            <motion.p
+              variants={fadeIn("up", 0.4)}
+              initial="hidden"
+              animate="show"
+              exit="hidden"
+              className="mb-4 max-w-[400px] mx-auto lg:mx-0"
+            >
               Lorem ipsum, dolor sit amet consectetur adipisicing elit.
               Repellendus assumenda cumque necessitatibus voluptas, aliquam
               doloribus ullam unde optio!
-            </p>
+            </motion.p>
           </div>
           {/* slider */}
-          <ServiceSlider />
+          <motion.div
+            variants={fadeIn("down", 0.6)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="w-full xl:max-w-[65%]"
+          >
+            <ServiceSlider />
+          </motion.div>
         </div>
       </div>
       <Bulb />
